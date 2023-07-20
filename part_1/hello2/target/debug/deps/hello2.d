b782a3e75b0ef9c71237f717bdd57426cfe85cdd
kusakabeka <n.kuzmingg@gmail.com>
2023-07-20 23:07:53 +0300

    hello2

--- part_1/hello2/target/debug/deps/hello2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\membr\OneDrive\Рабочий стол\rust-in-action\part_1\hello2\target\debug\deps\hello2.exe: src\main.rs
-
-C:\Users\membr\OneDrive\Рабочий стол\rust-in-action\part_1\hello2\target\debug\deps\hello2.d: src\main.rs
-
-src\main.rs:
--- part_1/hello2/target/debug/hello2.d
+++
@@ -1,1 +0,0 @@
-C:\Users\membr\OneDrive\Рабочий\ стол\rust-in-action\part_1\hello2\target\debug\hello2.exe: C:\Users\membr\OneDrive\Рабочий\ стол\rust-in-action\part_1\hello2\src\main.rs
